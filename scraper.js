1e56dc346816d135e4cdbcc6ffdf27e98cd557c3
hunihunor <hunihunor90@gmail.com>
2025-03-12 14:20:25 +0100

    reuw headless

--- scraper.js
+++ scraper.js
@@ -7,7 +7,7 @@ const fs = require("fs");
   const date = process.argv[4]; // Capture the date argument
 
   // Launch a browser instance
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   // Navigate to the dental clinic site
